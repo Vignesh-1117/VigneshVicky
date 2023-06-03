9b6d3d13b49e9e2174e516e78781c1a31306aebf
Vignesh-1117 <vignesh01117@gmail.com>
2023-06-03 23:06:16 +0530

    VigneshGit

---
+++ src/com/git/Sample.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Sample {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 3; i++) {
+			for (int j = 0; j <= 6; j++) {
+				System.out.println(j);
+			}
+		}
+
+	}
+}
